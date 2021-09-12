5f48e26535b61c8b2bfd7d26a41b512a740f1b8c
UnkJester <UnkJester@users.noreply.github.com>
2021-09-12 15:32:40 -0500

    replace old missed_states logic

---
+++ day026/states/states_main.py
@@ -0,0 +1,37 @@
+import turtle
+import pandas
+
+IMAGE = 'blank_states_img.gif'
+CSV_INPUT = '50_states.csv'
+
+s = turtle.Screen()
+s.title('U.S. States Game')
+s.addshape(IMAGE)
+turtle.shape(IMAGE)
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+t.speed('fastest')
+
+## Discover where the mouse is clicking
+# def get_mouse_click_coord(x, y):
+#     print(x, y)
+# s.onscreenclick(get_mouse_click_coord)
+states_csv = pandas.read_csv(CSV_INPUT)
+
+answer_list = []
+while len(answer_list) < 50:
+    s.update()
+    answer_state = s.textinput(title=f"{len(answer_list)}/50 states guessed", prompt='Please guess another state').title()
+    if answer_state == 'Exit':
+        missing = [state for state in states_csv['state'].to_list() if state not in answer_list]
+        output = pandas.DataFrame(missing)
+        output.columns = ['state']
+        output.to_csv('./missed_states.csv')
+        break
+    elif answer_state in states_csv['state'].to_list() and answer_state not in answer_list:
+        next_loc = states_csv[states_csv['state'] == answer_state]
+        t.goto(int(next_loc.x), int(next_loc.y))
+        t.write(arg=answer_state, align='center', font=("Arial", 8, "normal"))
+        answer_list.append(answer_state)
+s.exitonclick()
