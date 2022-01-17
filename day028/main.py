6860fa6f105940aec9e728930e0156fe6e90db95
UnkJester <UnkJester@users.noreply.github.com>
2022-01-16 20:40:38 -0600

    finish day 028

--- day028/main.py
+++ day028/main.py
@@ -6,44 +6,50 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+DARK_GREEN = "#125C13"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECK = '✔'
 reps = 0
+timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
-
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def timer_reset():
+    global reps, timer
+    window.after_cancel(timer)
+    lbl_timer.config(text='Timer')
+    lbl_completions.config(text='')
+    canvas.itemconfig(timer_text, text="00:00")
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
 
 def start_timer():
     global reps
     reps += 1
-    if reps in (1, 3, 5, 7):
-        # count_down(WORK_MIN * 60)
-        count_down(10)
-    elif reps in (2, 4, 6):
-        # count_down(SHORT_BREAK_MIN * 60)
-        count_down(5)
+    lbl_completions.config(text=CHECK*math.floor(reps/2))
+    if reps % 2 != 0:
+        lbl_timer.config(text="Work", fg=GREEN)
+        count_down(WORK_MIN * 60)
+        # count_down(5)
+    elif reps % 8 != 0:
+        lbl_timer.config(text="Short Rest", fg=PINK)
+        count_down(SHORT_BREAK_MIN * 60)
+        # count_down(2)
     else:
+        lbl_timer.config(text="Long Rest", fg=RED)
         count_down(LONG_BREAK_MIN * 60)
         reps = 0
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-
-
 def count_down(duration):
-    global reps
+    global reps, timer
     display_time = str(math.trunc(duration / 60)).zfill(2) + ':' + str(duration % 60).zfill(2)
     canvas.itemconfig(timer_text, text=display_time)
-    if duration > 0:
-        window.after(1000, count_down, duration-1)
+    if duration >= 0:
+        timer = window.after(1000, count_down, duration-1)
     else:
-        print('')
-        # if reps % 2 == 0:
-        #     print('what')
-        # start_timer()
-        # TODO: write check
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -63,10 +69,10 @@ canvas.grid(column=1, row=1)
 btn_start = tkinter.Button(width=2, height=1, text='Start', command=start_timer)
 btn_start.grid(column=0, row=2)
 
-btn_reset = tkinter.Button(width=2, height=1, text='Reset')
+btn_reset = tkinter.Button(width=2, height=1, text='Reset', command=timer_reset)
 btn_reset.grid(column=2, row=2)
 
-lbl_completions = tkinter.Label(text=CHECK, font=(FONT_NAME, 24, 'bold'), bg=YELLOW, fg=GREEN)
+lbl_completions = tkinter.Label(text="", font=(FONT_NAME, 24, 'bold'), bg=YELLOW, fg=GREEN)
 lbl_completions.grid(column=1, row=3)
 
 
